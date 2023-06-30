4b9cce5bcdf9a2cbca8c442d713051fdb11aa57f
Adarsh <adarshswaminath7@gmail.com>
2023-06-30 08:04:18 +0530

    Structure Changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
 import React from 'react'
-import Hero from './Components/Hero'
-import Ship from './Components/Ship'
-import Cards from './Components/Cards'
-import About from './Components/About'
-import History from './Components/History'
-import Contact from './Components/Contact'
-import Footer from './Components/Footer'
+import Components from './Components/Components'
 
 function App() {
   return (
     <div className='bg-[#111820]'>
-      <Hero/>
-      <Ship/>
-      <Cards/>
-      <About/>
-      <Contact/>
-      <Footer/>
+      <Components/>
     </div>
   )
 }
---
+++ src/Components/Components.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Hero from './Hero'
+import Ship from './Ship'
+import Cards from './Cards'
+import About from './About'
+import Contact from './Contact'
+import Footer from './Footer'
+
+function Components() {
+  return (
+    <div>
+        <Hero/>
+        <Ship/>
+        <Cards/>
+        <About/>
+        <Contact/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Components
